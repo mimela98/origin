98f079305fae6f7d8acafbf0ea37d9e68cfbaa8a
mimela98 <mimela98@gmail.com>
2019-07-08 09:14:39 +0000

    test

--- src/askpass.py
+++ src/askpass.py
@@ -9,11 +9,11 @@ from sys import argv
 from os import environ
 
 if argv[1] == "Username for 'https://github.com': ":
-    print environ['GIT_USERNAME']
+    print (environ['GIT_USERNAME'])
     exit()
 
 if argv[1] == "Password for 'https://%(GIT_USERNAME)s@github.com': " % environ:
-    print environ['GIT_PASSWORD']
+    print (environ['GIT_PASSWORD'])
     exit()
 
 exit(1)
